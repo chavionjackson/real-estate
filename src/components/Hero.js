da469ff473ce2b3ca570fc41f2ee3bb029187e8c
Chevy <75184977+chavionjackson@users.noreply.github.com>
2021-07-14 11:55:06 -0700

    Added more styling with images

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import styled, { css } from "styled-components/macro";
 import Button from './Button';
+import { RiArrowRightFill } from "react-icons/ri";
+import { IoArrowForward, IoArrowBack } from "react-icons/io5"
 
 const HeroSection = styled.section`
     height: 100vh;
@@ -19,11 +21,89 @@ const HeroWrapper = styled.div`
     position: relative;
 `;
 
-const HeroSlide = styled.div``;
-const HeroSlider = styled.div``;
-const HeroImage = styled.img``;
-const HeroContent = styled.div``;
+const HeroSlide = styled.div`
+    z-index: 1;
+    width: 100%;
+    height: 100%
+`;
 
+const HeroSlider = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  &::before {
+    content: "";
+    position: absolute;
+    z-index: 2;
+    width: 100%;
+    height: 100vh;
+    bottom: 0vh;
+    left: 0;
+    overflow: hidden;
+    opacity: 0.4;
+    background: linear-gradient(
+      0deg,
+      rgba(0, 0, 0, 0.2) 0%,
+      rgba(0, 0, 0, 0.2) 50%, 
+      rgba(0, 0, 0, 0.2) 100%,
+    );
+  }
+`;
+
+const HeroImage = styled.img`
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100vw;
+    height: 100vh;
+    object-fit: cover;
+`;
+
+const HeroContent = styled.div`
+
+`;
+
+const Arrow = styled(RiArrowRightFill)``;
+
+const SliderButtons = styled.div`
+    position: absolute;
+    bottom: 50px;
+    right: 50px;
+    display: flex;
+    z-index: 10;
+`;
+
+const arrowButtons = css`
+    width: 50px;
+    height: 50px;
+    color: #fff;
+    cursor: pointer;
+    background: #000d1a;
+    border-radius: 50px;
+    padding: 10px;
+    margin-right: 1rem;
+    user-select: none;
+    transition: 0.3s;
+
+    &:hover {
+        background: #cd853f;
+        transform: scale(1.05);
+    }
+`;
+
+const PrevArrow = styled(IoArrowBack)`
+    ${arrowButtons}
+`
+
+const NextArrow = styled(IoArrowForward)`
+    ${arrowButtons}
+`;
 
 const Hero = ({ slides }) => {
     return (
@@ -34,18 +114,23 @@ const Hero = ({ slides }) => {
                     return(
                         <HeroSlide key={id}>
                             <HeroSlider>
-                                <HeroImage />
+                                <HeroImage src={image} alt={alt}/>
                                 <HeroContent>
                                     <h1>{title}</h1>
                                     <p>{price}</p>
                                     <Button css={`max-width: 160px`} to={path} primary="true">
                                         {label}
+                                        <Arrow />
                                     </Button>
                                 </HeroContent>
                             </HeroSlider>
                         </HeroSlide>
                     )
                 })}
+                <SliderButtons>
+                    <PrevArrow />
+                    <NextArrow />
+                </SliderButtons>
             </HeroWrapper>
         </HeroSection>
     )
